75cbf28731de7022f07cca64ffd623363b6faf7c
PJxDev <pedrocruces@hotmail.es>
2022-12-31 16:21:24 +0100

    initial state

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="es">
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
       <Head />
-      <body>
+      <body className='flex || items-center || justify-center || bg-gradient-to-t from-softBlue to-softViolet || h-screen || bg-no-repeat'>
         <Main />
         <NextScript />
       </body>
