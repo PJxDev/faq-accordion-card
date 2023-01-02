bbea909fb508de6f6d80eb3b0ccbd74f2d6be491
PJxDev <pedrocruces@hotmail.es>
2023-01-02 16:18:08 +0100

    mobile ready

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="es">
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="true"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap"
+        rel="stylesheet"
+      />
       <Head />
-      <body className='bg-gradient-to-t from-softBlue to-softViolet || h-screen w-[100%] flex justify-center items-center'>
+      <body className="bg-gradient-to-t from-softBlue to-softViolet || h-screen w-[100%] flex justify-center items-center">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link
           rel="icon"
@@ -18,7 +18,7 @@ export default function Home() {
         />
         <title>Frontend Mentor | FAQ Accordion Card</title>
       </Head>
-      <main className="bg-lightGrayishBlue || text-xs text-center font-body || w-[80%] rounded-lg p-4 || flex flex-col justify-center items-center">
+      <main className=" bg-[white] || text-xs text-center font-body || m-4 max-w-5xl min-w-fit rounded-lg p-4 || flex flex-col justify-center items-center gap-2">
         <header className="relative pt-16">
           <Image
             alt="Woman with a blackboard"
@@ -30,72 +30,74 @@ export default function Home() {
             src={sha1}
             className="absolute top-2 scale-[300%]"
           />
-          <h1 className="text-2xl || font-bold">FAQ</h1>
+          <h1 className="text-2xl font-bold text-veryDarkDesaturatedBlue">
+            FAQ
+          </h1>
         </header>
-        <section className="flex flex-col gap-2">
-          <article>
-            <header className="flex flex-row justify-between">
+        <section className="flex flex-col gap-2 text-darkGrayishBlue">
+          <article className="relative">
+            <header className="flex flex-row gap-4 justify-between text-veryDarkGrayishBlue">
               <h2>How many team members can I invite?</h2>
               <button>
                 <Image alt="arrow" src={arrow} />
               </button>
             </header>
-            <p className="scale-0 absolute">
-                You can invite up to 2 additional users on the Free plan. There
-                is no limit on team members for the Premium plan.
-              </p>
+            <p className="absolute scale-0">
+              You can invite up to 2 additional users on the Free plan. There is
+              no limit on team members for the Premium plan.
+            </p>
           </article>
-          <hr />
+          <hr className=" text-lightGrayishBlue" />
           <article>
-            <header className="flex flex-row justify-between">
+            <header className="flex flex-row gap-4 justify-between text-veryDarkGrayishBlue">
               <h2>What is the maximum file upload size?</h2>
               <button>
                 <Image alt="arrow" src={arrow} />
-              </button>{" "}
+              </button>
             </header>
-            {/* <p>
-                No more than 2GB. All files in your account must fit your
-                allotted storage space.
-              </p> */}
+            <p className="absolute scale-0">
+              No more than 2GB. All files in your account must fit your allotted
+              storage space.
+            </p>
           </article>
-          <hr />
+          <hr className=" text-lightGrayishBlue" />
           <article>
-            <header className="flex flex-row justify-between">
+            <header className="flex flex-row gap-4 justify-between text-veryDarkGrayishBlue">
               <h2>How do I reset my password?</h2>
               <button>
                 <Image alt="arrow" src={arrow} />
-              </button>{" "}
+              </button>
             </header>
-            {/* <p>
-                Click “Forgot password” from the login page or “Change password”
-                from your profile page. A reset link will be emailed to you.
-              </p> */}
+            <p className="absolute scale-0">
+              Click “Forgot password” from the login page or “Change password”
+              from your profile page. A reset link will be emailed to you.
+            </p>
           </article>
-          <hr />
+          <hr className=" text-lightGrayishBlue" />
           <article>
-            <header className="flex flex-row justify-between">
+            <header className="flex flex-row gap-4 justify-between text-veryDarkGrayishBlue">
               <h2>Can I cancel my subscription?</h2>
               <button>
                 <Image alt="arrow" src={arrow} />
-              </button>{" "}
+              </button>
             </header>
-            {/* <p>
-                Yes! Send us a message and we’ll process your request no
-                questions asked.
-              </p> */}
+            <p className="absolute scale-0">
+              Yes! Send us a message and we’ll process your request no questions
+              asked.
+            </p>
           </article>
-          <hr />
+          <hr className=" text-lightGrayishBlue" />
           <article>
-            <header className="flex flex-row justify-between">
+            <header className="flex flex-row gap-4 justify-between text-veryDarkGrayishBlue">
               <h2>Do you provide additional support?</h2>
               <button>
                 <Image alt="arrow" src={arrow} />
-              </button>{" "}
+              </button>
             </header>
-            {/* <p>
-                Chat and email support is available 24/7. Phone lines are open
-                during normal business hours.
-              </p> */}
+            <p className="absolute scale-0">
+              Chat and email support is available 24/7. Phone lines are open
+              during normal business hours.
+            </p>
           </article>
         </section>
       </main>
